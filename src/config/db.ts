c03146f795b3e8e66910c08a12829794d4744438
Bruno <bruno.mestres@poscontrole.com.br>
2023-03-27 14:54:43 -0300

    model tutor

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,17 @@
-const knex = require('knex')({
-    client: 'pg',
-    connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'root',
-      password : 'root',
-      database : 'adopet'
-    }
-  })
+import { Sequelize } from 'sequelize'
+
+
+export const sequelize = new Sequelize('adopets', 'postgres', 'root', {
+    host: 'localhost',
+    dialect: 'postgres'
+})
+const run = async () => {
+ await sequelize.authenticate();
+}
+
+try {
+    run()
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
